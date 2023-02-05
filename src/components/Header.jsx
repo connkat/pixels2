53d30464e9302b264d57c940aafcec8f85b2399f
connkat <hi.connkat@gmail.com>
2023-02-04 18:15:13 -0700

    desktop

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import Grid from "@mui/material/Grid";
 import { Button, ThemeWrapper } from "retro-ui";
 
 import "../App.css";
@@ -8,33 +9,50 @@ function Header() {
   return (
     <ThemeWrapper>
       <div className="Header">
-        <img src={logo} className="logo" alt="logo" />
-        <div className="title">
-          <h1>Pixels and Pints!</h1>
-          <p>Home of Calgary's Oldest Tech Meetup</p>
-        </div>
-        <div className="buttons">
-          <div className="meetup-button">
-            <a
-              className="App-link"
-              href="https://www.meetup.com/pxandpints/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Button>Find the next meeting</Button>
-            </a>
-          </div>
-          <div className="discord-button">
-            <a
-              className="App-link"
-              href="https://discord.gg/y5JKKkHYgN"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Button>Join the Pixels Discord</Button>
-            </a>
-          </div>
-        </div>
+        <Grid container spacing={2}>
+          <Grid item xs={2}>
+            <img src={logo} className="logo" alt="logo" />
+          </Grid>
+          <Grid item xs={8}>
+            <div className="title">
+              <h1>
+                Pixels and Pints
+                <a
+                  href="https://pixels-og.netlify.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  !
+                </a>
+              </h1>
+              <p>Home of Calgary's Oldest Tech Meetup</p>
+            </div>
+          </Grid>
+          <Grid item xs={2}>
+            <div className="buttons">
+              <div className="meetup-button">
+                <a
+                  className="App-link"
+                  href="https://www.meetup.com/pxandpints/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Button>Find the next meeting</Button>
+                </a>
+              </div>
+              <div className="discord-button">
+                <a
+                  className="App-link"
+                  href="https://discord.gg/y5JKKkHYgN"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Button>Join the Pixels Discord</Button>
+                </a>
+              </div>
+            </div>
+          </Grid>
+        </Grid>
       </div>
     </ThemeWrapper>
   );
