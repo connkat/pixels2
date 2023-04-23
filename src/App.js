4cfad3ae7bfbc718ae35dd5d1ba08140add4ead4
connkat <hi.connkat@gmail.com>
2023-04-22 22:03:12 -0600

    added register and login links

--- src/App.js
+++ src/App.js
@@ -7,32 +7,74 @@ import joboffer from "./assets/joboffer.png";
 import "./App.css";
 
 function App() {
-  const body = `<body>
+	const body = `<body>
 <acuspire-job-widget></acuspire-job-widget>
 </body>`;
 
-  return (
-    <>
-      <Header />
-      <div className="App">
-        <div className="job-board-title">
-          <Grid container spacing={2}>
-            <Grid item xs={3} />
-            <Grid item xs={12} sm={6}>
-              <h3>Pixels Job Board</h3>
-            </Grid>
-            <Grid item xs={12} sm={3}>
-              <div className="sponsored">
-                <p>Sponsored by</p>
-                <img src={joboffer} className="joboffer" alt="joboffer" />
-              </div>
-            </Grid>
-          </Grid>
-        </div>
-        <div dangerouslySetInnerHTML={{ __html: body }} />{" "}
-      </div>
-    </>
-  );
+	return (
+		<>
+			<Header />
+			<div className="App">
+				<div className="job-board-title">
+					<Grid container spacing={2}>
+						<Grid item sm={3}>
+							<div className="sponsored">
+								<p>Sponsored by</p>
+								<img src={joboffer} className="joboffer" alt="joboffer" />
+							</div>
+						</Grid>
+						<Grid item xs={12} sm={6}>
+							<h3>Pixels Job Board</h3>
+						</Grid>
+            <Grid item xs={0} sm={1} />
+						<Grid item xs={10} sm={2}>
+							<div className="login">
+								<div className="login-wrapper">
+									<p>Employers</p>
+									<div className="button-container">
+										<button
+											href="https://pixels-recruiter.joboffer.ai/register"
+											target="_blank"
+											rel="noopener noreferrer"
+										>
+											Register
+										</button>
+										<button
+											href="https://pixels-recruiter.joboffer.ai/login"
+											target="_blank"
+											rel="noopener noreferrer"
+										>
+											Login
+										</button>
+									</div>
+								</div>
+								<div className="login-wrapper">
+									<p>Jobseekers</p>
+									<div className="button-container">
+										<button
+											href="https://pixels.joboffer.ai/register"
+											target="_blank"
+											rel="noopener noreferrer"
+										>
+											Register
+										</button>
+										<button
+											href="https://pixels.joboffer.ai/login"
+											target="_blank"
+											rel="noopener noreferrer"
+										>
+											Login
+										</button>
+									</div>
+								</div>
+							</div>
+						</Grid>
+					</Grid>
+				</div>
+				<div dangerouslySetInnerHTML={{ __html: body }} />{" "}
+			</div>
+		</>
+	);
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,45 +7,59 @@ import meetup from "../assets/meetup.png";
 import "./Header.css";
 
 export default function Header() {
-  return (
-    <div className="Header">
-      <Grid container spacing={2}>
-        <Grid item md={2} xs={3}>
-          <a href="http://pixelsyyc.com">
-            <img src={logo} className="logo" alt="logo" />
-          </a>
-        </Grid>
-        <Grid item md={8} xs={6}>
-          <div className="title">
-            <h1>Pixels &amp; Pints</h1>
-            <p>
-              Home of Calgary
-              <a
-                href="https://pixels-og.netlify.app/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                '
-              </a>
-              s Oldest Tech Meetup
-            </p>
-          </div>
-        </Grid>
-        <Grid item md={2} xs={3}>
-          <div className="links">
-            <div className="links">
-              <a href="https://discord.gg/y5JKKkHYgN" alt="discord">
-                <img src={discord} className="link" alt="discord" />
-                <p className="desktop_label">Discord</p>
-              </a>
-              <a href="https://www.meetup.com/pxandpints/" alt="meetup">
-                <img src={meetup} className="link" alt="meetup" />
-                <p className="desktop_label">Meetup</p>
-              </a>
-            </div>
-          </div>
-        </Grid>
-      </Grid>
-    </div>
-  );
+	return (
+		<div className="Header">
+			<Grid container spacing={2}>
+				<Grid item md={2} xs={3}>
+					<a
+						href="http://pixelsyyc.com"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<img src={logo} className="logo" alt="logo" />
+					</a>
+				</Grid>
+				<Grid item md={8} xs={6}>
+					<div className="title">
+						<h1>Pixels &amp; Pints</h1>
+						<p>
+							Home of Calgary
+							<a
+								href="https://pixels-og.netlify.app/"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								'
+							</a>
+							s Oldest Tech Meetup
+						</p>
+					</div>
+				</Grid>
+				<Grid item md={2} xs={3}>
+					<div className="links">
+						<div className="links">
+							<a
+								href="https://discord.gg/y5JKKkHYgN"
+								target="_blank"
+								rel="noopener noreferrer"
+								alt="discord"
+							>
+								<img src={discord} className="link" alt="discord" />
+								<p className="desktop_label">Discord</p>
+							</a>
+							<a
+								href="https://www.meetup.com/pxandpints/"
+								target="_blank"
+								rel="noopener noreferrer"
+								alt="meetup"
+							>
+								<img src={meetup} className="link" alt="meetup" />
+								<p className="desktop_label">Meetup</p>
+							</a>
+						</div>
+					</div>
+				</Grid>
+			</Grid>
+		</div>
+	);
 }
